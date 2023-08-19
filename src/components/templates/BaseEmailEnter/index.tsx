bc5e0a1efccbf5b2772dfd8f53b1f649806e545b
VitorPrates <111801858+VitorPrates@users.noreply.github.com>
2023-08-19 18:46:57 -0300

    landing-page mobile ver.1
    Remoção de css inline, atualização da parte visual mobile, adição de header e footer, correções visuais.

--- src/components/templates/BaseEmailEnter/index.tsx
+++ src/components/templates/BaseEmailEnter/index.tsx
@@ -1,7 +1,6 @@
 import { nanoid } from "nanoid";
 import { useState } from "react";
 import styles from "./index.module.scss";
-
 export type BaseEmailEnter = {
   children?: React.ReactNode;
   // Override these as needed - Remember, optional props goes at the end.
@@ -9,48 +8,22 @@ export type BaseEmailEnter = {
 
 function BaseEmailEnter({ children }: BaseEmailEnter) {
   return (
-    <div>
-      <form
-        style={{
-          border: "1px solid",
-          maxWidth: "300px",
-          display: "flex",
-          boxShadow:"8px 8px #3F0957"
-        }}
-      >
+    <div className={styles.container}>
+      <form className={styles.entry_form_1}>
         <input
           placeholder="Digite seu E-mail Aqui"
           style={{
-            fontFamily: "Bebas Neue",
-            maxWidth: "300px",
-            border: "0px solid",
-            padding: 4,
-            fontSize: "1.7em",
-            width: "100%",
+            
           }}
         />
         <button
           type="submit"
           aria-label="add-message-to-list-btn"
-          style={{
-            padding: 4,
-            backgroundColor: "rgb(0,0,0,0)",
-            border: "0px solid",
-            fontSize: "1.3em",
-            fontFamily: "Bebas Neue",
-            borderLeft:"1px solid"
-          }}
-          data-test="btn-submit-msg"
         >
           Enviar
         </button>
       </form>
-      <p
-        style={{
-          fontFamily: "monospace",
-          margin:"10px 0px 0px 0px"
-        }}
-      >
+      <p>
         Só mandaremos e-mail sobre o lançamento do site, não se preocupe!
       </p>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import BaseEmailEnter from "@/components/templates/BaseEmailEnter";
-import BaseTemplateWithHooks from "@/components/templates/BaseTemplateWithHooks";
-import BasicBlockText from "@/components/templates/BasicBlockText";
+// import logo from "./images/logo1.svg"
+import styles from "./index.module.scss"
 import Head from "next/head";
-
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,37 +13,48 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main
-        className="titulos"
-        style={{
-          padding: "30px",
-        }}
-      >
-        <h1
-          style={{
-            textAlign: "left",
-            margin: "40px 0px -15px 0px",
-            fontSize: "2.7em",
-          }}
-          data-test="msg-welcome"
-        >
-          RECEBA ATUALIZAÇÕES<br/>SOBRE NOSSO LANÇAMENTO
-        </h1>
+      <main className={styles.main_container}>
+        <header className={styles.main_header}>
+          <div className={styles.logo_titulo}>
+            <Image
+              className={styles.logo_main_header}
+              priority
+              src="/images/logo.svg"
+              height={125}
+              width={125}
+              alt="Logo PackShoP"
+            />
+            <h1>
+              PACK <br />
+              SHOP
+            </h1>
+          </div>
+          <h1 className={styles.welcome}>SEJA BEM-VINDO(A)</h1>
+        </header>
+        <h2 className={styles.main_titulo}>
+          RECEBA ATUALIZAÇÕES
+          <br />
+          SOBRE NOSSO LANÇAMENTO!
+        </h2>
         <br />
-        <div
-          className="titulos"
-          style={{
-            width: "100%",
-            display: "flex",
-            flexDirection: "column",
-            gap: 16,
-            alignItems: "left",
-          }}
-        >
+        <div className={styles.send_email}>
           <BaseEmailEnter />
-          <BasicBlockText />
-          {/* <BaseTemplateWithHooks /> */}
         </div>
+        <div className={styles.about_us}>
+          <h2>PackShop, encontre o que você precisa, venda e sem limites.</h2>
+          <div className={styles.block_copy}>
+            <p>
+              Redefinindo sua experiência de compras com produtos bem
+              detalhados, filtros bem definidos, tendo uma das menores taxas e
+              maiores benefícios do mercado.
+            </p>
+          </div>
+        </div>
+        <footer className={styles.end_main_infos}>
+          <div className={styles.real_footer}>
+            <h3>CROWARE TECH LTDA &copy;</h3>
+          </div>
+        </footer>
       </main>
     </>
   );
