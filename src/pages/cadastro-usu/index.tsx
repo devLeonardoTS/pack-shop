f3dff809236465e8a512b64e8a8b361f2506fe14
VitorPrates <111801858+VitorPrates@users.noreply.github.com>
2023-11-05 21:02:36 -0300

    corrigindo e modularizando
    correção no formulário de login, senha inicia como não visualizável. Modularização do header simples

--- src/pages/cadastro-usu/index.tsx
+++ src/pages/cadastro-usu/index.tsx
@@ -1,3 +1,12 @@
+import Top_Infos from "@/components/templates/topinfostemplate";
+import styles from './cadastro-usu.module.scss'
+
+
 export default function Home() {
-  return <h1>heio</h1>;
+  return (
+    <main>
+      <Top_Infos/>
+      <h1>heio</h1>
+    </main>
+  );
 }
