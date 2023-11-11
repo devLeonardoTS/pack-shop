a71c00abae68ce0848dba047b7dce9fb06a43acc
Leonardo T. S <leonardo_takanobu@hotmail.com>
2023-11-11 17:32:41 -0300

    patch: seller-drawer styling

--- src/components/content/Dashboard/SellerDrawer/index.tsx
+++ src/components/content/Dashboard/SellerDrawer/index.tsx
@@ -6,6 +6,7 @@ import {
 } from "@/common/hooks/useUserData";
 import { useBusinessDashboardStore } from "@/common/stores/BusinessDashboardStore";
 import { useUserSessionStore } from "@/common/stores/UserSessionStore";
+import RippleButton from "@/components/common/RippleButton";
 import { Drawer } from "@mui/material";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -43,22 +44,26 @@ function SellerDrawer({ children }: SellerDrawerProps) {
       classes={{ root: style["drawer"], paper: style["drawer"] }}
     >
       <div className={style.header}>
-        <div className={style["image-box"]}>
-          <img
-            src={image?.imageUrl || Defaults.Placeholders.avatar}
-            width={128}
-            height={128}
-          />
+        <div className={style["image-area"]}>
+          <div className={style["image-box"]}>
+            <img
+              src={image?.imageUrl || Defaults.Placeholders.avatar}
+              width={128}
+              height={128}
+              alt={`Logomarca da ${nomeFantasia}` || "Logomarca da loja"}
+              title={`Logomarca da ${nomeFantasia}` || "Logomarca da loja"}
+            />
+          </div>
         </div>
         <div className={style["text-group"]}>
           <p className={style["title"]}>{razaoSocial}</p>
           <p className={style["store"]}>{nomeFantasia}</p>
         </div>
-        <menu className={style.menu}>
+        <menu className={style["horizontal-menu"]}>
           <li>
-            <button onClick={() => {}}>
-              <FaUserCog />
-            </button>
+            <RippleButton className={`ripple-btn rounded`} onClick={() => {}}>
+              <FaUserCog className={style["btn-icon"]} />
+            </RippleButton>
           </li>
         </menu>
       </div>
@@ -66,18 +71,31 @@ function SellerDrawer({ children }: SellerDrawerProps) {
       <hr />
       <menu className={style["main-menu"]}>
         <li>
-          <button onClick={() => setDashboardContent()}>DASHBOARD</button>
+          <RippleButton
+            className={`ripple-btn`}
+            onClick={() => setDashboardContent()}
+          >
+            DASHBOARD
+          </RippleButton>
         </li>
         <li>
-          <button onClick={() => setDashboardContent(<ProductFormArea />)}>
+          <RippleButton
+            className={`ripple-btn`}
+            onClick={() => setDashboardContent(<ProductFormArea />)}
+          >
             PRODUTOS
-          </button>
+          </RippleButton>
         </li>
       </menu>
       <hr />
       <menu className={style["secondary-menu"]}>
         <li>
-          <button onClick={() => router.push("/")}>VOLTAR</button>
+          <RippleButton
+            className={`ripple-btn rounded-sm`}
+            onClick={() => router.push("/")}
+          >
+            VOLTAR
+          </RippleButton>
         </li>
       </menu>
       <div className={style["footer"]}></div>
